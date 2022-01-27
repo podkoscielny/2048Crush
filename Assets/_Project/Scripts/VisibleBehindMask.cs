74fc7a121c8d3e0f4d984306ad193514861b87e0
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-01-27 21:42:57 +0100

    Hide tiles behind transparent object

---
+++ Assets/_Project/Scripts/VisibleBehindMask.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class VisibleBehindMask : MonoBehaviour
+{
+    [SerializeField] Material maskMaterial;
+
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+
+        renderer.sharedMaterial.renderQueue = maskMaterial.renderQueue - 1;
+    }
+
+}
--- Assets/_Project/Shaders/Mask.shader
+++ Assets/_Project/Shaders/Mask.shader
@@ -1,16 +1,18 @@
-Shader "Unlit/Mask"
+Shader "Custom/DepthReserve"
 {
     Properties
     {
-        
     }
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
+        Tags { "RenderType" = "Opaque" "Queue" = "Geometry-1" }
+        LOD 100
+
+        Blend Zero One
 
         Pass
         {
-           Zwrite Off
+
         }
     }
 }
