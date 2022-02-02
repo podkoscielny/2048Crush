82668f02889a716b85cc14c95725beddab8a3857
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-02-02 21:36:08 +0100

    Add quit game method

--- Assets/_Project/Scripts/SceneController.cs
+++ Assets/_Project/Scripts/SceneController.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 namespace Crush2048
 {
     public class SceneController : MonoBehaviour
     {
         public void GoToPlayScene() => SceneManager.LoadScene("Main");
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+
+#endif
+        }
     }
 }
