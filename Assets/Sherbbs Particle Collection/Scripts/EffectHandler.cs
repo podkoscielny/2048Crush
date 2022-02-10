0db5f561c5f24a4fd821a22da9d2b40679b8dcea
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-02-10 13:02:52 +0100

    Adjust match particles animation

--- Assets/Sherbbs Particle Collection/Scripts/EffectHandler.cs
+++ Assets/Sherbbs Particle Collection/Scripts/EffectHandler.cs
@@ -8,11 +8,26 @@ namespace Crush2048
     public class EffectHandler : MonoBehaviour
     {
         [SerializeField] ObjectPool objectPool;
+        [SerializeField] EffectColor effectColor;
+        [SerializeField] List<ParticleSystem> particleSystems;
 
         private const float DEACTIVATE_DELAY = 0.4f;
 
-        private void OnEnable() => Invoke(nameof(DeactivateEffect), DEACTIVATE_DELAY);
+        private void OnEnable()
+        {
+            SetEffectColor();
+            Invoke(nameof(DeactivateEffect), DEACTIVATE_DELAY);
+        }
 
         private void DeactivateEffect() => objectPool.AddToPool(Tags.MatchEffect, gameObject);
+
+        private void SetEffectColor()
+        {
+            //foreach (ParticleSystem particleSystem in particleSystems)
+            //{
+            //    var main = particleSystem.main;
+            //    main.startColor = new ParticleSystem.MinMaxGradient(effectColor.Color);
+            //}
+        }
     }
 }
