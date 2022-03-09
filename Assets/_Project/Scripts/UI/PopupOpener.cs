f9c33bd99121a98e613d29830c3a6cd3d94395ab
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-03-09 20:07:22 +0100

    Adjust popup opener animation

--- Assets/_Project/Scripts/UI/PopupOpener.cs
+++ Assets/_Project/Scripts/UI/PopupOpener.cs
@@ -11,18 +11,9 @@ namespace Crush2048
         [SerializeField] RectTransform objectToAnimate;
         [Range(0, 2f)] [SerializeField] float moveAmount;
 
-        private Vector3 _initialPosition;
-        private Vector3 _initialScale;
-        private Vector3 _loweredScale;
+        private Vector3 _punchScale = new Vector3(0.1f, 0.1f, 0.1f);
 
-        private const float ANIMATION_DURATION = 0.22f;
-
-        private void Start()
-        {
-            _initialPosition = objectToAnimate.position;
-            _initialScale = objectToAnimate.localScale;
-            _loweredScale = _initialScale * 0.9f;
-        }
+        private const float ANIMATION_DURATION = 0.4f;
        
         public void ShowPopup()
         {
@@ -36,15 +27,8 @@ namespace Crush2048
         {
             UIToFade.alpha = 0;
 
-            Vector3 startPosition = new Vector3(_initialPosition.x, _initialPosition.y - moveAmount, _initialPosition.z);
-            Vector3 midPosition = new Vector3(_initialPosition.x, _initialPosition.y + (moveAmount / 2), _initialPosition.z);
-
-            objectToAnimate.position = startPosition;
-            objectToAnimate.localScale = _loweredScale;
-
             UIToFade.DOFade(1, ANIMATION_DURATION).SetEase(Ease.OutCirc);
-            objectToAnimate.DOMove(midPosition, ANIMATION_DURATION / 2).OnComplete(() => objectToAnimate.DOMove(_initialPosition, ANIMATION_DURATION)).SetEase(Ease.OutCirc);
-            objectToAnimate.DOScale(_initialScale, ANIMATION_DURATION).SetEase(Ease.OutCirc);
+            objectToAnimate.DOPunchScale(_punchScale, ANIMATION_DURATION).SetEase(Ease.OutCirc);
         }
     }
 }
