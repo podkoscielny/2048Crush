e54926079e4e0b81368742706b6be16c6fe1e2c7
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-03-14 12:24:20 +0100

    Add Open scene transition

---
+++ Assets/_Project/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Crush2048
+{
+    public class SceneTransition : MonoBehaviour
+    {
+        [SerializeField] RectTransform leftDoor;
+        [SerializeField] RectTransform rightDoor;
+
+
+    }
+}
