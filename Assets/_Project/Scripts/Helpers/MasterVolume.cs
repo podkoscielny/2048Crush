07553ad9c04485665f1834e06a6bfef7c696dbcf
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-04-20 20:01:41 +0200

    Adjust tile types spawn probability for 4x4 and 5x5 grid sizes

--- Assets/_Project/Scripts/Helpers/MasterVolume.cs
+++ Assets/_Project/Scripts/Helpers/MasterVolume.cs
@@ -8,10 +8,9 @@ namespace Crush2048
 {
     public static class MasterVolume
     {
-        private static readonly float _changeVolumeDuration = 0.7f;
         private static Tween _muteTween;
-
         private static bool _isMuting = false;
+        private static readonly float _changeVolumeDuration = 0.7f;
 
         public static void MuteTest(Action callback = null)
         {
@@ -19,12 +18,6 @@ namespace Crush2048
 
             _muteTween = DOTween.To(() => AudioListener.volume, x => AudioListener.volume = x, 0, _changeVolumeDuration)
                 .OnComplete(() => MuteCallback(callback));
-        }
-
-        private static void MuteCallback(Action AdditionalCallback = null)
-        {
-            _isMuting = false;
-            AdditionalCallback.Invoke();
         }
 
         public static void UnMuteTest()
@@ -36,6 +29,12 @@ namespace Crush2048
             }
 
             DOTween.To(() => AudioListener.volume, x => AudioListener.volume = x, 1, _changeVolumeDuration);
+        }
+
+        private static void MuteCallback(Action AdditionalCallback = null)
+        {
+            _isMuting = false;
+            AdditionalCallback.Invoke();
         }
     }
 }
